746c5f356555280c605416dae68db3ff11d8e735
pawelprochnicki <pawelprochnicki@users.noreply.github.com>
2024-03-17 19:05:12 +0100

    main weather component (empty)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { ApiKeyInterceptor } from './services/apikey.interceptor';
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
+import { WeatherMainComponent } from './components/weather-main/weather-main.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderBarComponent, FooterBarComponent],
+  declarations: [AppComponent, HeaderBarComponent, FooterBarComponent, WeatherMainComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ApiKeyInterceptor, multi: true },
---
+++ src/app/components/weather-main/weather-main.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WeatherMainComponent } from './weather-main.component';
+
+describe('WeatherMainComponent', () => {
+  let component: WeatherMainComponent;
+  let fixture: ComponentFixture<WeatherMainComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WeatherMainComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WeatherMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
