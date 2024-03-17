27f4457830652290de1aa019b275df27fd72285d
pawelprochnicki <pawelprochnicki@users.noreply.github.com>
2024-03-17 20:00:27 +0100

    add toastr library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { ToastrModule } from 'ngx-toastr';
 import { ApiKeyInterceptor } from './services/apikey.interceptor';
-
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 import { WeatherMainComponent } from './components/weather-main/weather-main.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderBarComponent, FooterBarComponent, WeatherMainComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    HeaderBarComponent,
+    FooterBarComponent,
+    WeatherMainComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
+  ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ApiKeyInterceptor, multi: true },
   ],
--- src/app/components/weather-main/weather-main.component.ts
+++ src/app/components/weather-main/weather-main.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { OpenWeatherMapService } from 'src/app/services/open-weather-map.service';
 
 @Component({
   selector: 'cw-weather-main',
   templateUrl: './weather-main.component.html',
-  styleUrls: ['./weather-main.component.scss']
+  styleUrls: ['./weather-main.component.scss'],
 })
 export class WeatherMainComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private openWeatherMapService: OpenWeatherMapService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
+    this.openWeatherMapService
+      .getWeatherForCity('4567', 'metric')
+      .pipe(
+        catchError((error) => {
+          this.toastr.error(error, 'Error');
+          console.error('Error in WeatherMainComponent:', error);
+          return of(null);
+        })
+      )
+      .subscribe((data) => {
+        if (data) {
+          console.log(data);
+        }
+      });
   }
-
 }
